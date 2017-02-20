f0334a0bbc66f684e58bd15bb3a9f90c5b4cf5c8
michael <mihai@mandrescu.co>
2017-02-20 19:27:57 +0000

    Unit testing

--- MongoRouter/MongoRouter.py
+++ MongoRouter/MongoRouter.py
@@ -13,6 +13,10 @@ class MongoRouter(object):
             "host",
             {"host": "localhost", "port": 27017}
         )
+        self.mongo_client_settings = settings.get(
+            "client_settings",
+            {}
+        )
         self.routes = settings.get("routes", {})
 
         self.create_collections = create_collections
@@ -27,15 +31,20 @@ class MongoRouter(object):
             else:
                 logging.warning("[MongoRouter] No routes provided and collection creation disabled.")
 
+        client_settings = dict(
+            self.mongo_host.items() + self.mongo_client_settings.items()
+        )
         self.client = MongoClient(
-            self.mongo_host
+            client_settings
         )
 
     def _route(self, desired_collection):
         try:
-            pass
-        except:
-            pass
+            return self.client[self.routes[desired_collection]["db"]][self.routes[desired_collection]["col"]]
+        except KeyError:
+            if self.create_collections:
+                logging.info("Creating collection %s in mongo_router_db." % desired_collection)
+                return self.client["mongo_router_db"][desired_collection]
 
         raise ValueError("Route to %s not found." % desired_collection)
 
@@ -44,9 +53,9 @@ class MongoRouter(object):
 
     def route(self, desired_collection):
         if self.default_custom_routes:
-            self.custom_routes(desired_collection)
+            return self.custom_routes(desired_collection)
         else:
-            self._route(desired_collection)
+            return self._route(desired_collection)
 
 
 import unittest
@@ -54,10 +63,39 @@ import unittest
 
 class TestMongoRouter(unittest.TestCase):
     def setUp(self):
+        # Create this DB and collection at every test
         self.test_collection = MongoClient()["test_db"]["test_db"]
 
+        self.test_routes = {
+            "test": {
+                "db": "test_db",
+                "col": "test_db"
+            }
+        }
+
     def tearDown(self):
+        # Tear down the test DB and collection
         self.test_collection.drop()
 
-    def test_route(self):
-        pass
+    def test_basic_routing(self):
+        # "Manually" insert some test item
+        self.test_collection.insert_one({
+            "test_id": "tid",
+            "test": "success"
+        })
+
+        router = MongoRouter(settings={
+            "routes": self.test_routes
+        })
+
+        self.assertEquals(
+            "success",
+            router.route("test").find_one({"test_id": "tid"}).get("test", None)
+        )
+
+        router.route("test").insert_one({"test_id": "tid_2", "test": "success"})
+
+        self.assertEquals(
+            "success",
+            router.route("test").find_one({"test_id": "tid_2"}).get("test", None)
+        )
