9346bf9cbf697146b65468c6ed0ba551a5b60f5b
michael <mihai@mandrescu.co>
2017-06-16 23:41:25 +0100

    Basic test for routing via config dict.

--- MongoRouter.py
+++ MongoRouter.py
@@ -2,9 +2,19 @@ from MongoProxy import MongoProxy
 
 import pymongo
 
+import socket
+
+
+def get_host(config):
+    # Mongo host as a function of the local hostname
+    hostname = socket.gethostname()
+    return config.get(hostname, "localhost")
+
 
 class MongoRouter():
-    def __init__(self):
+    def __init__(self,
+                 config=None,
+                 config_file=None):
 
-        self.safe_conn = MongoProxy(pymongo.MongoClient())
+        self.safe_conn = MongoProxy(pymongo.MongoClient(host=get_host(config)))
 
--- MongoRouterTests.py
+++ MongoRouterTests.py
@@ -5,12 +5,11 @@ from MongoRouter import MongoRouter
 
 class MongoRouterTests(unittest.TestCase):
     def setUp(self):
-
-        self.router = MongoRouter()
+        self.router = MongoRouter({
+            "MichaelsMBP": "localhost",
+            "ip-172-31-7-91": "172.31.11.139"
+        })
 
     def test(self):
-        self.router.safe_conn.conn.mongo_router_db.test.insert({"one": True})
-
-
-# suite = unittest.TestLoader().loadTestsFromTestCase(MongoRouterTests)
-# unittest.TextTestRunner().run(suite)
+        import socket
+        self.router.safe_conn.conn.mongo_router_db.test.insert({"inserted_from": socket.gethostname()})
